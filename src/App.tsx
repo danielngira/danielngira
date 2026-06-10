700d67dab83292e531fc8d4d700feb30973e8fe9
Daniel Ngira <81624602+danielngira@users.noreply.github.com>
2026-06-10 11:32:54 -0500

    implementing task bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,64 @@ import folder from './assets/images/folder.png';
 import computer from './assets/images/computer.png';
 import harddrive from './assets/images/hard-drive.png';
 import storage from './assets/images/storage.png';
+import internet from './assets/images/internet.png';
 
 function App() {
 
   return (
     <>
+      {/* Home page components */}
       <div>
+
+        {/* Icons */}
         <div className='container'>
+
+            {/*Projects*/}
             <div className='folderContainer'>
                 <img src={folder} alt='Folder Icon' className='icon'/>
                 <p className='folderName'>Projects</p>
             </div>
             
+            {/*Biography about me*/}
             <div className='folderContainer'>
                 <img src={computer} alt='Computer Icon' className='icon'/>
                 <p className='folderName'>My Computer</p>
             </div>
 
+            {/*Media and Hobbies*/}
             <div className='folderContainer'>
                 <img src={harddrive} alt='Hard Drive Icon' className='icon'/>
                 <p className='folderName'>Media</p>
             </div>
 
+            {/*Resume*/}
             <div className='folderContainer'>
                 <img src={storage} alt='Storage Icon' className='icon'/>
                 <p className='folderName'>Resume</p>
             </div>
-            
 
+            {/*Contact me*/}
+            <div className='folderContainer'>
+                <img src={internet} alt='Internet Explorer Icon' className='icon'/>
+                <p className='folderName'>Contact Me</p>
+            </div>
+            
         </div>
 
+        {/*Taskbar*/}
         <div>
             <div className='taskbar'>
+                {/*Start Button*/}
+                <div className='start'>
+                    <p>Start</p>
+                </div>
+
+                {/*Clock*/}
+                <div className='clock'>
+                    <p>11:05</p>
+                </div>
             </div>
+            
         </div>
       </div>
     </>
