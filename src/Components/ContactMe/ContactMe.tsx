7f00b6a8277dce2c7923354bd6f5be4f3649eed5
Daniel Ngira <81624602+danielngira@users.noreply.github.com>
2026-07-06 16:33:12 -0700

    updated contact me

--- src/Components/ContactMe/ContactMe.tsx
+++ src/Components/ContactMe/ContactMe.tsx
@@ -1,7 +1,25 @@
+import "./ContactMe.css";
+import email from "../../assets/images/email.png";
+import linkedin from "../../assets/images/linkedin.png";
+import github from "../../assets/images/github.png";
+
 function ContactMe() {
   return (
     <>
-      <p> Update coming soon...</p>
+      <div className="contactContainer">
+        <a href="mailto:danielngira@uchicago.edu" target="_blank">
+          <img src={email} alt="Email Icon" className="contactIcon" />
+          <p>Email</p>
+        </a>
+        <a href="https://www.linkedin.com/in/danielngira" target="_blank">
+          <img src={linkedin} alt="LinkedIn Icon" className="contactIcon" />
+          <p>LinkedIn</p>
+        </a>
+        <a href="https://www.github.com/danielngira" target="_blank">
+          <img src={github} alt="GitHub Icon" className="contactIcon" />
+          <p>GitHub</p>
+        </a>
+      </div>
     </>
   );
 }
